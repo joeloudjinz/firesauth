0063a32ae4c56e51d659e0ac178b57fee6c0cfa2
Addoun Abdullah <33733877+joeloudjinz@users.noreply.github.com>
2019-12-11 14:25:55 +0100

    Implementing login logic (#4)

--- scripts/auth.js
+++ scripts/auth.js
@@ -16,6 +16,24 @@ signupForm.addEventListener('submit', (event) => {
         });
 });
 
+// Login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password)
+        .then((credentials) => {
+            const modal = document.querySelector('#modal-login');
+            M.Modal.getInstance(modal).close();
+            loginForm.reset();
+        }).catch((error) => {
+            console.log('error :', error);
+        });
+});
+
 // Logout
 const logoutBtn = document.querySelector('#logout');
 logoutBtn.addEventListener('click', (event) => {
